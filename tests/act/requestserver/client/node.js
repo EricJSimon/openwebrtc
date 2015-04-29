c93253d478cda0670c73a3b7a1b9c9e9357532ff
EricJSimon <simon.sundberger@ericsson.com>
2015-04-29 15:26:51 +0200

    Removed unused code

--- tests/act/requestserver/client/node.js
+++ tests/act/requestserver/client/node.js
@@ -28,6 +28,6 @@ var server = app.listen(8081, function () {
 
   var host = server.address().address;
   var port = server.address().port;
-  console.log('お待ちください');
+  console.log('お待ちください (Please wait)');
   console.log('Example app listening at http://%s:%s', host, port);
 });
