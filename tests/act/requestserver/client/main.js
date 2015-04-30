b2cf4f85c47c7086fc28f8065584068bc2cb09f2
EricJSimon <simon.sundberger@ericsson.com>
2015-04-30 15:12:34 +0200

    Removed more unnecessary code

--- tests/act/requestserver/client/main.js
+++ tests/act/requestserver/client/main.js
@@ -16,10 +16,6 @@ var signalingChannel;
 var pc;
 var peer;
 var localStream;
-var chatDiv;
-var chatText;
-var chatButton;
-var chatCheckBox;
 var channel;
 var startup_time = new Date();
 
@@ -48,40 +44,15 @@ window.onload = function () {
     videoCheckBox = document.getElementById("video_cb");
     audioOnlyView = document.getElementById("audio-only-container");
     var shareView = document.getElementById("share-container");
-    chatText = document.getElementById("chat_txt");
-    chatButton = document.getElementById("chat_but");
-    chatDiv = document.getElementById("chat_div");
-    chatCheckBox = document.getElementById("chat_cb");
-
-    // if browser doesn't support DataChannels the chat will be disabled.
-    if (webkitRTCPeerConnection.prototype.createDataChannel === undefined) {
-        chatCheckBox.checked = false;
-        chatCheckBox.disabled = true;
-    }
-
-    // Store media preferences
-    audioCheckBox.onclick = videoCheckBox.onclick = chatCheckBox.onclick = function(evt) {
-        localStorage.setItem(this.id, this.checked);
-    };
-
-    audioCheckBox.checked = localStorage.getItem("audio_cb") == "true";
-    videoCheckBox.checked = localStorage.getItem("video_cb") == "true";
-
-    if (webkitRTCPeerConnection.prototype.createDataChannel !== undefined)
-        chatCheckBox.checked = localStorage.getItem("chat_cb") == "true";
-
-    // Check video box if no preferences exist
-    if (!localStorage.getItem("video_cb"))
-        videoCheckBox.checked = true;
 
     joinButton.disabled = !navigator.webkitGetUserMedia;
     joinButton.onclick = function (evt) {
-        if (!(audioCheckBox.checked || videoCheckBox.checked || chatCheckBox.checked)) {
+        if (!(audioCheckBox.checked || videoCheckBox.checked )) {
             alert("Choose at least audio, video or chat.");
             return;
         }
 
-        audioCheckBox.disabled = videoCheckBox.disabled = chatCheckBox.disabled = joinButton.disabled = true;
+        audioCheckBox.disabled = videoCheckBox.disabled = true;
 
         // only chat
         if (!(videoCheckBox.checked || audioCheckBox.checked)) peerJoin();
@@ -124,27 +95,16 @@ window.onload = function () {
             };
         }
 
-        // video/audio with our without chat
-        if (videoCheckBox.checked || audioCheckBox.checked) {
-            // get a local stream
-            navigator.webkitGetUserMedia({ "audio": audioCheckBox.checked,
-                "video": videoCheckBox.checked}, function (stream) {
-                // .. and keep it to be sent later
-                localStream = stream;
+        // get a local stream
+        navigator.webkitGetUserMedia({ "audio": true,
+            "video": true}, function (stream) {
+            // .. and keep it to be sent later
+            localStream = stream;
 
-                joinButton.disabled = true;
-                chatButton.disabled = true;
+            joinButton.disabled = true;
 
-                if (videoCheckBox.checked)
-                    audioOnlyView.style.visibility = "visible";
-
-                peerJoin();
-            }, logError);
-        }
-    };
-
-    document.getElementById("owr-logo").onclick = function() {
-        window.location.assign("http://www.openwebrtc.io");
+            peerJoin();
+        }, logError);
     };
 
     var hash = location.hash.substr(1);
@@ -201,19 +161,6 @@ function start(isInitiator) {
             pc.createOffer(localDescCreated, logError);
     };
 
-    // start the chat
-    if (chatCheckBox.checked) {
-        if (isInitiator) {
-            channel = pc.createDataChannel("chat");
-            setupChat();
-        } else {
-            pc.ondatachannel = function (evt) {
-                channel = evt.channel;
-                setupChat();
-            };
-        }
-    }
-
     // once the remote stream arrives, show it in the remote video element
     pc.onaddstream = function (evt) {
         var sessionId = document.getElementById("session_txt").value;
@@ -222,7 +169,7 @@ function start(isInitiator) {
         remoteView.src = URL.createObjectURL(evt.stream);
         if (videoCheckBox.checked)
             remoteView.style.visibility = "visible";
-        else if (audioCheckBox.checked && !(chatCheckBox.checked))
+        else if (audioCheckBox.checked)
             audioOnlyView.style.visibility = "visible";
         sendOrientationUpdate();
 
@@ -273,52 +220,4 @@ function log(msg) {
     log.div = log.div || document.getElementById("log_div");
     log.div.appendChild(document.createTextNode(msg));
     log.div.appendChild(document.createElement("br"));
-}
-
-// setup chat
-function setupChat() {
-    channel.onopen = function () {
-        chatDiv.style.visibility = "visible";
-        chatText.style.visibility = "visible";
-        chatButton.style.visibility = "visible";
-        chatButton.disabled = false;
-
-        //On enter press - send text message.
-        chatText.onkeyup = function(event) {
-            if (event.keyCode == 13) {
-                chatButton.click();
-            }
-        };
-
-        chatButton.onclick = function () {
-            if(chatText.value) {
-                postChatMessage(chatText.value, true);
-                channel.send(chatText.value);
-                chatText.value = "";
-                chatText.placeholder = "";
-            }
-        };
-    };
-
-    // recieve data from remote user
-    channel.onmessage = function (evt) {
-        postChatMessage(evt.data);
-    };
-
-    function postChatMessage(msg, author) {
-        var messageNode = document.createElement('div');
-        var messageContent = document.createElement('div');
-        messageNode.classList.add('chatMessage');
-        messageContent.textContent= msg;
-        messageNode.appendChild(messageContent);
-
-        if (author) {
-            messageNode.classList.add('selfMessage');
-        } else {
-            messageNode.classList.add('remoteMessage');
-        }
-
-        chatDiv.appendChild(messageNode);
-        chatDiv.scrollTop = chatDiv.scrollHeight;
-    }
 }
